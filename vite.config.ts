4a56588a6e84701401e7a657ce71efe2db8a0327
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-08-08 23:50:51 +0530

    Add proxy server settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,21 @@ import { defineConfig } from "vite";
 import path from "node:path";
 import electron from "vite-plugin-electron/simple";
 import react from "@vitejs/plugin-react";
-
+import tsConfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [
     react(),
+    tsConfigPaths(),
     tailwindcss(),
     electron({
       main: {
